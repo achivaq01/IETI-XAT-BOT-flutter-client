73fb15ef39ed47363a080f26c0db85d4c9e05454
joelberzalgithub <120808626+joelberzalgithub@users.noreply.github.com>
2024-01-26 15:18:24 +0100

    Revised and finalized Util Message class

--- lib/util_message.dart
+++ lib/util_message.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 
 class Message {
-  final int _messageId;
-  int author;
-  String messageText;
-  Image? image;
+  final int _messageId; // Enter final que representa l'id del missatge
+  int author; // Enter que representa qui ha enviat el missatge (IA o usuari)
+  String messageText; // Cadena de text que representa el contingut del missatge
+  Image? image; // Imatge que ve adjunta amb el missatge (en cas de fer servir Llava)
 
   Message(this._messageId, this.messageText, this.author, this.image);
 
